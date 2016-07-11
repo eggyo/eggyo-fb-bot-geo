61924ee4c41f3f47c680c661a1ace029c6dfe56e
eggyo <eggyoyo4859@gmail.com>
2016-07-11 20:10:37 +0700

    v.2.0.0

--- app.js
+++ app.js
@@ -745,7 +745,7 @@ function callParseServerCloudCode(methodName,requestMsg) {
 };
 
 function callback(error, response, body) {
-  console.log("code:"+response.statusCode);
+  console.log("response:"+response);
   if (!error && response.statusCode == 200) {
     var info = JSON.parse(body);
     console.log(info);
