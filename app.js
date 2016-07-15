97371f76ca3e51a3b71ce84d6a88c0400545aba7
Eggyothin Pila <eggyo@users.noreply.github.com>
2016-07-15 18:31:59 +0700

    fix bug

--- app.js
+++ app.js
@@ -898,8 +898,8 @@ function trainingCommand(msg,res) {
   msg = msg.replace("#ask ","");
   msg = msg.replace(" #ans ",":");
   var msgs = msg.split(":");
-  var replyDatas = msgs[1].replace(",","','");
-  var data = '{"msg":"'+msgs[0]+'","replyMsg":"['+replyDatas+']"}';
+  var replyDatas = msgs[1].replace(",","","");
+  var data = '{"msg":"'+msgs[0]+'","replyMsg":["'+replyDatas+'"]}';
   callParseServerCloudCode("botTraining",data,function(response){
     console.log(response);
     res(response);
