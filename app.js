ab84a8e0083d7125e52701f78500188235799db5
eggyo <eggyoyo4859@gmail.com>
2016-07-23 22:11:07 +0700

    process elev

--- app.js
+++ app.js
@@ -836,7 +836,7 @@ function getGeoConvert(lat,long,responseMsg) {
 }
 function getGoogleElev(lat,long,responseMsg) {
   var options = {
-  url: 'https://maps.googleapis.com/maps/api/elevation/json?locations='+lat+','+long+ '&key=AIzaSyAn9cWoce9zGEfGjDzMg6r_uTTUw3WoMOg'
+  url: 'https://maps.googleapis.com/maps/api/elevation/json?locations='+lat+','+long+ '&key=AIzaSyAn9cWoce9zGEfGjDzMg6r_uTTUw3WoMOg',
   method: 'GET'
   };
   function callback(error, response, body) {
