9428b0e1fed2ad3c6bda01c0d8f22494ee7ad905
eggyo <eggyoyo4859@gmail.com>
2016-07-24 16:43:46 +0700

    add user checking

--- app.js
+++ app.js
@@ -849,7 +849,7 @@ function userCheck(senderID) {
         callUserProfileAPI(senderID,function(responseUser){
           // add new fbID to mLab
           console.log("new user :"+senderID+" userFB:"+JSON.stringify(responseUser));
-          addNewUserToDatabase(responseUser,function(responseNewUser){
+          addNewUserToDatabase(JSON.stringify(responseUser),function(responseNewUser){
 
           });
         });
